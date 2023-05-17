02e9f1966edded1f4da7451197d70a8f9b3a1f26
mitchellmisayah <mitchellmisayah@gmail.com>
2023-05-16 20:04:36 -0500

    dbcontext done and migration

--- ASPNETMVCCRUD/Program.cs
+++ ASPNETMVCCRUD/Program.cs
@@ -1,7 +1,12 @@
+using ASPNETMVCCRUD.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//inject dbcontext
+builder.Services.AddDbContext<MVCDemoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MvcDemoConnectionString")));
 
 var app = builder.Build();
 
